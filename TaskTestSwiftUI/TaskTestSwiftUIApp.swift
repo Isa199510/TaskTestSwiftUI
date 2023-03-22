ebcba8c428b357791248a4c8916f7703f1d5d276
Isa <isa.arbuhov@mail.ru>
2023-03-22 21:04:06 +0300

    Initial Commit

---
+++ TaskTestSwiftUI/TaskTestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTestSwiftUIApp.swift
+//  TaskTestSwiftUI
+//
+//  Created by Иса on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskTestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
